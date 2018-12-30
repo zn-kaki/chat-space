6238ee0b5ef150e36c5383f150d892ff2a160e1f
zn-kaki <zn-kaki>
2018-12-30 22:48:31 +0900

    ルーティング設定
    Resourcesを用いて、新規作成機能:new、グループ保存:create、編集画面:edit、更新:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for
   root 'messages#index'
   resources :users, only: [:edit, :update]
-    resources :groups, only: [:index, :new, :create, :edit, :update] do
+    resources :groups, only: [:index,  :create, :edit, :update] do
+      resources :messages, only: [:index, :create]
 end
 end
