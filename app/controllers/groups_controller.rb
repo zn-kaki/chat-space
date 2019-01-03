f8e464bf4da4a50fd560feb310b61cd74af4bd76
zn-kaki <zn-kaki>
2019-01-03 23:17:03 +0900

    グループ編集機能
    グループ機能編集機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   def index
   end
 
@@ -14,9 +16,22 @@ class GroupsController < ApplicationController
     else
       render :new
     end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
+
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
   end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+end
