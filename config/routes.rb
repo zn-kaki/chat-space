01ce9a8ace2549c2e1353440511c15fa8d7f9d78
zn-kaki <zn-kaki>
2019-01-06 11:53:19 +0900

    グループ編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
+  devise_for :users
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
+    resources :messages, only: [:index]
   end
 end
