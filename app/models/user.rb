2fe16c66136ebf9656cc96bd2c9e980df92e810e
zn-kaki <zn-kaki>
2019-01-08 13:56:26 +0900

    モデル機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
 
   has_many :messages
   has_many :group_users
-  has_many :groups, through: :group_users
+  has_many :groups, through: :groups_users
 end
