eaef0daf661417da0e157138b1c054d1441b171b
zn-kaki <zn-kaki>
2019-01-09 16:13:42 +0900

    db nameカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20190106030749) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
