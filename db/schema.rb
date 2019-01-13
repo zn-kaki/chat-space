003989a9d7c8b4d3d9e77ec06c9866c57427b3f4
zn-kaki <zn-kaki>
2019-01-13 09:56:17 +0900

    スキーマファイル

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20190106030749) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
-    t.string   "imaget"
+    t.string   "image"
     t.integer  "group_id"
     t.integer  "user_id"
     t.datetime "created_at", null: false
