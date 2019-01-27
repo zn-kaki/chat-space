8ae53a8eb30d5857c60eeb104b124087ba74ac1d
zn-kaki <zn-kaki>
2019-01-27 12:00:17 +0900

    メッセージ非同期通信

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,4 +1,23 @@
 $(function() {
+  //メッセージ表示のHTML生成
+  function buildHTML(message){
+    var html = `<div class="message">
+                 <div class="upper-message">
+                  <div class="upper-message__user-name">
+                    ${ message.user_name}
+                  </div>
+                  <div class="upper-message__date">
+                      ${message.time}
+                  </div>
+                 </div>
+                 <div class="lower-message>
+                     <p class="lower-message__content">
+                       ${ message.content }
+                     </p>
+                 </div>
+                </div>`;
+  return html;
+  }
   //メッセージの非同期通信
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -13,6 +32,12 @@ $(function() {
      dataType: 'json',
      processData: false,
      contentType: false
+    })
+    .done(function(data) {
+      .var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message').val("");
+      $('.form__submit').prop('disabled',false);
     })
   })
 })
