d8cf59135a79d16b21e2d5e88278fe29b41f4d47
zn-kaki <zn-kaki>
2019-01-27 11:07:34 +0900

    メッセージ非同期通信

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,37 @@
+class MessagesController < ApplicationController
+  before_action :set_group
+
+  def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
+    respond_to do |format|
+       format.html
+       format.json
+    end
+  end
+
+  def create
+    @message = @group.messages.new(message_params[:group_id])
+    if @message.save
+      respond_to do |format|
+      redirect_to group_messages_path(group), notice: 'メッセージが送信されました'
+       format.html {redirect_to group_messages_path(@group)}
+       format.json
+    end
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+end
