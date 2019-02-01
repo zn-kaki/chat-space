531437fb3040de75fd4570fc7702fb2c7390c8e2
zn-kaki <zn-kaki>
2019-02-01 09:46:05 +0900

    Add kaki

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+    resources :users, only: [:index]
   end
 end
