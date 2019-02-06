0b43748eaad5f9a1bba656e0705094ba28378f7c
zn-kaki <zn-kaki>
2019-02-06 11:41:47 +0900

    メッセージコントローラー

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,10 +11,9 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = @group.messages.new(message_params[:group_id])
+    @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
-      redirect_to group_messages_path(group), notice: 'メッセージが送信されました'
        format.html {redirect_to group_messages_path(@group)}
        format.json
     end
