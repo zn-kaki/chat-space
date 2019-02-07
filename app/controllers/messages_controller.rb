1ce522662b7ebd8627b26809c01e4a83530ad904
zn-kaki <zn-kaki>
2019-02-07 20:51:13 +0900

    メッセージコントローラー

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,14 +6,14 @@ class MessagesController < ApplicationController
     @messages = @group.messages.includes(:user)
     respond_to do |format|
        format.html
-       format.json
+       format.json  { @new_message = Message.where('id > ?', params[:message][:id]) }
     end
   end
 
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      respond_to do |format|
+       respond_to do |format|
        format.html {redirect_to group_messages_path(@group)}
        format.json
     end
@@ -27,7 +27,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :imaget).merge(user_id: current_user.id)
   end
 
   def set_group
