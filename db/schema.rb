f78a2f233e932dccefaa40894d07fd3023686188
zn-kaki <zn-kaki>
2019-02-07 20:57:08 +0900

    カラム

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20190106030749) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
-    t.string   "image"
+    t.integer  "imaget_will_change"
     t.integer  "group_id"
     t.integer  "user_id"
     t.datetime "created_at", null: false
@@ -40,14 +40,15 @@ ActiveRecord::Schema.define(version: 20190106030749) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                                null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "name",                   limit: 11, default: "", null: false
+    t.string   "encrypted_password",                default: "", null: false
+    t.string   "email",                                          null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.string   "emailCopy",                         default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
