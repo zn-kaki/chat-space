0789f69d7b62ad49e8877d1111a6bca0e36c8f6d
zn-kaki <zn-kaki>
2019-03-15 14:24:08 +0900

    Factory.bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,5 +64,5 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, type: :controller
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 end
