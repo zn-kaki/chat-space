e05c6271ceefae0b34ac170b80a01b33cb0ef13b
zn-kaki <zn-kaki>
2019-03-15 13:27:56 +0900

    Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "chat-space"
-set :repo_url, "git@github.com:zn-kaki/chat-space.git"
+set :application, 'chat-space'
+set :repo_url, 'git@github.com:zn-kaki/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -10,10 +10,10 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['kakiuser.pem']
+                  keys: ['.ssh/kakiuser.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-set :unicorn_config_path, -> { "#{currnt_path}/config/unicorn.rb" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 after 'deploy:publishing', 'deploy:restart'
